6860bce01ecd0fa9e5a35f6b2530325f11b3f5fd
Ricardo Mendonça Filho <ricardojmf2005@gmail.com>
2024-07-28 13:43:59 -0300

    Primeiro commit da API de Ricardo

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+Product.create(
+  name: "Gabinete Gamer Cooler Master Sneaker X Red, Mini-Tower, Fonte SFX850W, Water Cooler Flux 360mm, Cabo Riser PCI-e, 1 Fan MF120 Halo, ABK-SXNN-S38L3-R1",
+  price: 8299.99,
+  image: "https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/a/b/abk-sxnn-s38l3-r13.jpg",
+  is_promotion: false
+)
+
+Product.create(
+  name: "Mesa Gamer Mancer Arcane, 136cm, RGB, Preto, MCR-ARC-RGB",
+  price: 539.99,
+  image: "https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/m/c/mcr-arc-rgb5.jpg",
+  is_promotion: true
+)
